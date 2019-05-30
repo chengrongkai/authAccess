9ad243e85d03dadd0972fa3c008133cb4d749bb5
chengrongkai <15623826795@163.com>
2019-05-30 22:08:23 +0800

    generator

---
+++ model/src/main/java/com/crk/dubbo/authAccess/model/entity/SysUser.java
@@ -0,0 +1,63 @@
+package com.crk.dubbo.authAccess.model.entity;
+
+public class SysUser {
+    private Integer userId;
+
+    private String userName;
+
+    private Integer phone;
+
+    private String password;
+
+    private String salt;
+
+    private Integer state;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt == null ? null : salt.trim();
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+}
